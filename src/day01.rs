9134ebaa8d419fa6ad58871cb37a66034c38e6dc
Sam Clark <sam.h.clark@gmail.com>
2021-12-01 22:56:34 -0600

    chore: Refactor 2021-01
    Stealing some things that I learned on Reddit.
    
    * I didn't know about `lines()` which helped get rid of the splitting I
      was doing myself
    * I had no idea that `windows()` was a thing, but it's perfect for this.
    * The only logic optimization is that you don't need to consider the
      middle two elements in the 3-element sliding window.
      (b + c + d) > (a + b + c) is the same as (d) > (a)

--- src/day01.rs
+++ src/day01.rs
@@ -4,70 +4,32 @@ use std::error::Error;
 
 #[allow(dead_code)]
 pub fn part1() -> Result<(), Box<dyn Error>> {
-    let input: &str = include_str!("../inputs/day01.in");
+    let input: Vec<i64> = include_str!("../inputs/day01.in")
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
 
-    let split_input = input.split_whitespace();
-
-    let mut vec = Vec::<u32>::new();
-    for num in split_input {
-        let parsed: u32 = num.parse::<u32>()?;
-        vec.push(parsed);
-    }
-
-    let mut previous = 0;
-    let mut increases = 0;
-
-    for depth in vec {
-        if previous != 0 && depth > previous {
-            increases += 1;
-        }
-        previous = depth;
-    }
+    let increases = input
+        .windows(2)
+        .filter(|window| window[1] > window[0])
+        .count();
 
     println!("Total number of depth increases: {}", increases);
 
     Ok(())
 }
 
-struct Window {
-    a: u32,
-    b: u32,
-    c: u32,
-}
-
 #[allow(dead_code)]
 pub fn part2() -> Result<(), Box<dyn Error>> {
-    let input: &str = include_str!("../inputs/day01.in");
+    let input: Vec<i64> = include_str!("../inputs/day01.in")
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
 
-    let split_input = input.split_whitespace();
-
-    let mut vec = Vec::<u32>::new();
-    for num in split_input {
-        let parsed: u32 = num.parse::<u32>()?;
-        vec.push(parsed);
-    }
-
-    let mut window = Window { a: 0, b: 0, c: 0 };
-    let mut increases = 0;
-
-    for depth in vec {
-        if window.a == 0 {
-            window.a = depth;
-        } else if window.b == 0 {
-            window.b = depth;
-        } else if window.c == 0 {
-            window.c = depth;
-        } else {
-            let prev = window.a + window.b + window.c;
-            let curr = window.b + window.c + depth;
-            if curr > prev {
-                increases += 1;
-            }
-            window.a = window.b;
-            window.b = window.c;
-            window.c = depth;
-        }
-    }
+    let increases = input
+        .windows(4)
+        .filter(|window| window[3] > window[0])
+        .count();
 
     println!("Total number of depth increases: {}", increases);
 
