e1fb201aec47e0329b0e5d2c170e8f8d61a464a9
Sam Clark <sam.h.clark@gmail.com>
2021-12-03 18:35:20 -0600

    chore: Refactor 2021-03
    I still don't really like the solutions. I wish I could have droppped
    down to bit banging earlier in the solution, that would be nicer, I
    think.

--- src/day03.rs
+++ src/day03.rs
@@ -3,47 +3,46 @@
 use std::error::Error;
 
 #[allow(dead_code)]
-pub fn part1(input: &str) -> Result<u64, Box<dyn Error>> {
-    let mut sums: [i64; 12] = [0; 12];
+pub fn part1(puzzle_input: &str) -> Result<u64, Box<dyn Error>> {
+    const NUMBER_OF_BITS: usize = 12;
+    const THRESHOLD: u32 = 500;
+    let mut number_of_ones_by_bit: [u32; NUMBER_OF_BITS] = [0; NUMBER_OF_BITS];
 
-    input.lines().for_each(|line| {
+    puzzle_input.lines().for_each(|line| {
         line.chars().enumerate().for_each(|(i, c)| {
             if c == '1' {
-                sums[i] += 1
+                number_of_ones_by_bit[i] += 1;
             }
         })
     });
 
-    let mut gamma: String = "".to_string();
-    let mut epsilon: String = "".to_string();
-    for num_occurances in sums {
-        if num_occurances > 500 {
-            gamma = format!("{}1", gamma);
-            epsilon = format!("{}0", epsilon);
+    let mut gamma: String = String::with_capacity(NUMBER_OF_BITS);
+    let mut epsilon: String = String::with_capacity(NUMBER_OF_BITS);
+    for number_of_ones in number_of_ones_by_bit {
+        if number_of_ones > THRESHOLD {
+            gamma.push('1');
+            epsilon.push('0');
         } else {
-            gamma = format!("{}0", gamma);
-            epsilon = format!("{}1", epsilon);
+            gamma.push('0');
+            epsilon.push('1');
         }
     }
 
     let gamma_number = u64::from_str_radix(&gamma, 2)?;
     let epsilon_number = u64::from_str_radix(&epsilon, 2)?;
 
-    println!("Gamma number: {}", gamma);
-    println!("Epsilon number: {}", epsilon);
-
-    let result = gamma_number * epsilon_number;
-    println!("Result: {}", result);
-    Ok(result)
+    let puzzle_answer = gamma_number * epsilon_number;
+    println!("Puzzle answer: {}", puzzle_answer);
+    Ok(puzzle_answer)
 }
 
 #[allow(dead_code)]
 pub fn part2(input: &str) -> Result<u64, Box<dyn Error>> {
-    let oxygen_generator_rating: u64 = calculate_oxygen_generator_rating(&input)?;
-    let co2_scrubber_rating: u64 = calculate_co2_scrubber_rating(&input)?;
+    let oxygen_generator_rating: u64 = calculate_oxygen_generator_rating(input)?;
+    let co2_scrubber_rating: u64 = calculate_co2_scrubber_rating(input)?;
 
     let puzzle_answer = oxygen_generator_rating * co2_scrubber_rating;
-    println!("Puzzle result: {}", puzzle_answer);
+    println!("Puzzle answer: {}", puzzle_answer);
     Ok(puzzle_answer)
 }
 
@@ -52,9 +51,8 @@ fn calculate_oxygen_generator_rating(input: &str) -> Result<u64, Box<dyn Error>>
     let input_vec: Vec<String> = input.lines().map(|s| s.to_string()).collect();
 
     let mut result: u64 = 1;
-    let mut filtered = input_vec.clone();
+    let mut filtered = input_vec;
     for i in 0..12 {
-        println!("Bit {}; Current list: {:?}", i, filtered);
         filtered = check_and_filter_oxygen_readings(&filtered, i)?;
         if filtered.len() == 1 {
             let result_string = filtered.get(0).unwrap();
@@ -72,7 +70,6 @@ fn check_and_filter_oxygen_readings(
 ) -> Result<Vec<String>, Box<dyn Error>> {
     let total_readings = input_vec.len();
     let filter_threshold = total_readings - (total_readings / 2);
-    println!("Filter threshold: {}", filter_threshold);
 
     let mut number_of_ones = 0;
     input_vec.iter().for_each(|line| {
@@ -86,7 +83,6 @@ fn check_and_filter_oxygen_readings(
     } else {
         '1'
     };
-    println!("Bit {}; Most common: {}", bit_to_check, most_common);
 
     let output: Vec<String> = input_vec
         .iter()
@@ -102,9 +98,8 @@ fn calculate_co2_scrubber_rating(input: &str) -> Result<u64, Box<dyn Error>> {
     let input_vec: Vec<String> = input.lines().map(|s| s.to_string()).collect();
 
     let mut result: u64 = 1;
-    let mut filtered = input_vec.clone();
+    let mut filtered = input_vec;
     for i in 0..12 {
-        println!("Bit {}; Current list: {:?}", i, filtered);
         filtered = check_and_filter_c02_readings(&filtered, i)?;
         if filtered.len() == 1 {
             let result_string = filtered.get(0).unwrap();
@@ -120,12 +115,8 @@ fn check_and_filter_c02_readings(
     input_vec: &[String],
     bit_to_check: usize,
 ) -> Result<Vec<String>, Box<dyn Error>> {
-    let total_readings = input_vec.len();
-    let filter_threshold = total_readings - (total_readings / 2);
-    println!("Filter threshold: {}", filter_threshold);
-
-    let mut number_of_ones = 0;
-    let mut number_of_zeros = 0;
+    let mut number_of_ones: u32 = 0;
+    let mut number_of_zeros: u32 = 0;
     input_vec.iter().for_each(|line| {
         if line.chars().nth(bit_to_check).unwrap() == '0' {
             number_of_zeros += 1;
@@ -139,7 +130,6 @@ fn check_and_filter_c02_readings(
     } else {
         '1'
     };
-    println!("Bit {}; Least common: {}", bit_to_check, least_common);
 
     let output: Vec<String> = input_vec
         .iter()
@@ -154,24 +144,24 @@ fn check_and_filter_c02_readings(
 mod day03_tests {
     use super::*;
 
-// Change the `part1` function to use 5 bits instead of 12 for this to pass
-//     #[test]
-//     fn test_case_part_1() {
-//         let input: &str = "00100
-// 11110
-// 10110
-// 10111
-// 10101
-// 01111
-// 00111
-// 11100
-// 10000
-// 11001
-// 00010
-// 01010";
+    // Change the `part1` function to use 5 bits instead of 12 for this to pass
+    //     #[test]
+    //     fn test_case_part_1() {
+    //         let input: &str = "00100
+    // 11110
+    // 10110
+    // 10111
+    // 10101
+    // 01111
+    // 00111
+    // 11100
+    // 10000
+    // 11001
+    // 00010
+    // 01010";
 
-//         assert_eq!(198, part1(input).unwrap());
-//     }
+    //         assert_eq!(198, part1(input).unwrap());
+    //     }
 
     #[test]
     fn test_case_calculate_oxygen_generator_rating() {
