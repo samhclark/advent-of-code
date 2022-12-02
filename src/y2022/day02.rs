00ce59214ff73cab28b72b44db4746b7418c5f26
Sam Clark <sam.h.clark@gmail.com>
2022-12-02 10:14:55 -0600

    Scaffolding for 2022, day 2

---
+++ src/y2022/day02.rs
@@ -0,0 +1,33 @@
+// Day 2: Rock Paper Scissors
+
+use itertools::Itertools;
+
+static INPUT: &str = include_str!("../../inputs/2022/day02.in");
+
+#[allow(dead_code)]
+pub fn part01() {
+    println!("Answer is: {}", todo!());
+}
+
+#[allow(dead_code)]
+pub fn part02() {
+    println!("Answer is: {}", todo!());
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_case_part_1() {
+        let input = "";
+        assert_eq!(true, true);
+    }
+
+    #[test]
+    fn test_case_part_2() {
+        let input = "";
+        assert_eq!(true, true);
+    }
+}
