5b32489cb77e296cb05b5b53f516d7e150febdca
Sam Clark <sam.h.clark@gmail.com>
2022-12-02 10:46:32 -0600

    2022, Day 2, Part 1

--- src/y2022/day02.rs
+++ src/y2022/day02.rs
@@ -6,12 +6,38 @@ static INPUT: &str = include_str!("../../inputs/2022/day02.in");
 
 #[allow(dead_code)]
 pub fn part01() {
-    println!("Answer is: {}", todo!());
+    println!("Answer is: {}", calc_score_for_strategy(INPUT));
 }
 
 #[allow(dead_code)]
 pub fn part02() {
     println!("Answer is: {}", todo!());
+}
+
+fn calc_score_for_strategy(strategy: &str) -> u64 {
+    let mut total_score: u64 = 0;
+    for round in strategy.lines() {
+        let (theirs, mine) = round.split_once(' ').unwrap();
+        println!("New round: theirs: {}, mine: {}", theirs, mine);
+        let theirs = theirs.chars().nth(0).unwrap() as u8;
+        let mine = mine.chars().nth(0).unwrap() as u8;
+        let theirs = theirs - b'A' + 1;
+        let mine = mine - b'X' + 1;
+        println!("as bytes: theirs: {}, mine: {}", theirs, mine);
+
+        if mine == theirs {
+            total_score += 3;
+            println!("draw")
+        } else if (mine == 1 && theirs == 3) 
+            || (mine == 2 && theirs == 1)
+            || (mine == 3 && theirs == 2) {
+            total_score += 6;
+            println!("win")
+        }
+        total_score += mine as u64;
+    }
+    
+    total_score
 }
 
 #[cfg(test)]
@@ -21,8 +47,10 @@ mod test {
 
     #[test]
     fn test_case_part_1() {
-        let input = "";
-        assert_eq!(true, true);
+        let input = "A Y
+B X
+C Z\n";
+        assert_eq!(calc_score_for_strategy(input), 15);
     }
 
     #[test]
