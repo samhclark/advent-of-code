38fcf8c916a7312520bfc4dae4293aa845e46c51
Sam Clark <sam.h.clark@gmail.com>
2023-12-02 16:59:12 -0600

    Refactoring and Clippy

--- src/y2023/day02.rs
+++ src/y2023/day02.rs
@@ -1,4 +1,4 @@
-use std::{str::FromStr, num::ParseIntError, cmp::max};
+use std::{cmp::max, num::ParseIntError, str::FromStr};
 
 static INPUT: &str = include_str!("../../inputs/2023/day02.in");
 
@@ -6,7 +6,7 @@ static INPUT: &str = include_str!("../../inputs/2023/day02.in");
 struct Handful {
     pub red_qty: u64,
     pub green_qty: u64,
-    pub blue_qty: u64
+    pub blue_qty: u64,
 }
 
 impl FromStr for Handful {
@@ -15,21 +15,33 @@ impl FromStr for Handful {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut red: u64 = 0;
         let mut green: u64 = 0;
-        let mut blue: u64 = 0; 
+        let mut blue: u64 = 0;
         let trimmed = s.trim();
         let quantities = trimmed.split(',');
         for count_of_one_color in quantities {
-            let quantity_str: String = count_of_one_color.chars().filter(char::is_ascii_digit).collect::<String>();
-            let quantity = quantity_str.parse::<u64>().expect("digits always fit in 64 bits");
-            let color_str: String = count_of_one_color.chars().filter(char::is_ascii_alphabetic).collect::<String>();
+            let quantity_str: String = count_of_one_color
+                .chars()
+                .filter(char::is_ascii_digit)
+                .collect::<String>();
+            let quantity = quantity_str
+                .parse::<u64>()
+                .expect("digits always fit in 64 bits");
+            let color_str: String = count_of_one_color
+                .chars()
+                .filter(char::is_ascii_alphabetic)
+                .collect::<String>();
             match color_str.as_str() {
                 "red" => red = quantity,
                 "green" => green = quantity,
                 "blue" => blue = quantity,
-                _ => unreachable!("colors are always red, green, or blue")
+                _ => unreachable!("colors are always red, green, or blue"),
             };
         }
-        Ok(Self { red_qty: red, green_qty: green, blue_qty: blue })
+        Ok(Self {
+            red_qty: red,
+            green_qty: green,
+            blue_qty: blue,
+        })
     }
 }
 
@@ -46,29 +58,18 @@ pub fn part02() {
 }
 
 fn sum_ids_of_possible_games(records_of_games: &str, num_games: u64) -> u64 {
-    let max_red = 12;
-    let max_green = 13;
-    let max_blue = 14;
-
+    // To let the loop exit early, we'll assume all games are possible, and subtract the IDs of
+    // games that we determine are impossible.
     let mut sum_of_ids: u64 = (1..=num_games).sum();
     'thisgame: for (id, game) in records_of_games.lines().enumerate() {
         // strip game header
-        let (_header, all_rounds) = game.split_once(':').expect("all games have a header");
-        let rounds = all_rounds.split(';');
-        'thisround: for round in rounds {
+        let (_header, rounds) = game.split_once(':').expect("all games have a header");
+
+        // there's probably an idiomatic way to split this into "handfuls.any { exceeds_max }..."
+        for round in rounds.split(';') {
             let handful = Handful::from_str(round).expect("always parseable");
-            if handful.red_qty > max_red {
-                sum_of_ids = sum_of_ids - (id as u64 + 1);
-                continue 'thisgame;
-            }
-
-            if handful.green_qty > max_green {
-                sum_of_ids = sum_of_ids - (id as u64 + 1);
-                continue 'thisgame;
-            }
-
-            if handful.blue_qty > max_blue {
-                sum_of_ids = sum_of_ids - (id as u64 + 1);
+            if exceeds_max_allowed(&handful) {
+                sum_of_ids -= 1 + id as u64;
                 continue 'thisgame;
             }
         }
@@ -77,28 +78,34 @@ fn sum_ids_of_possible_games(records_of_games: &str, num_games: u64) -> u64 {
     sum_of_ids
 }
 
+const fn exceeds_max_allowed(handful: &Handful) -> bool {
+    let max_red = 12;
+    let max_green = 13;
+    let max_blue = 14;
+
+    handful.red_qty > max_red || handful.green_qty > max_green || handful.blue_qty > max_blue
+}
+
 fn sum_of_powers_of_min_sets(records_of_games: &str) -> u64 {
-    let mut total_power = 0;
-    for game in records_of_games.lines() {
-        // strip game header
-        let (_header, rounds) = game.split_once(':').expect("all games have a header");
+    records_of_games
+        .lines()
+        .map(|game| {
+            // strip game header
+            let (_header, rounds) = game.split_once(':').expect("all games have a header");
 
-        let mut min_red = 0;
-        let mut min_green = 0;
-        let mut min_blue = 0;
-        
-        for round in rounds.split(';') {
-            let handful = Handful::from_str(round).expect("always parseable");
-            min_red = max(handful.red_qty, min_red);
-            min_green = max(handful.green_qty, min_green);
-            min_blue = max(handful.blue_qty, min_blue);
-        }
+            let mut min_red = 0;
+            let mut min_green = 0;
+            let mut min_blue = 0;
+            for round in rounds.split(';') {
+                let handful = Handful::from_str(round).expect("always parseable");
+                min_red = max(handful.red_qty, min_red);
+                min_green = max(handful.green_qty, min_green);
+                min_blue = max(handful.blue_qty, min_blue);
+            }
 
-        let power = min_red * min_green * min_blue;
-        total_power += power;
-    }
-
-    total_power
+            min_red * min_green * min_blue
+        })
+        .sum()
 }
 #[cfg(test)]
 mod test {
