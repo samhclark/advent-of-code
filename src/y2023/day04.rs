92ef9898e97f000d24a8e4a1e88f7d85fd387894
Sam Clark <sam.h.clark@gmail.com>
2023-12-04 22:35:22 -0600

    2023, day 4, part 2

--- src/y2023/day04.rs
+++ src/y2023/day04.rs
@@ -2,8 +2,6 @@
 
 use std::collections::HashSet;
 
-use itertools::Itertools;
-
 static INPUT: &str = include_str!("../../inputs/2023/day04.in");
 
 #[allow(dead_code)]
@@ -14,7 +12,7 @@ pub fn part01() {
 
 #[allow(dead_code)]
 pub fn part02() {
-    let answer = "";
+    let answer = how_many_after_playing(INPUT);
     println!("Puzzle answer: {answer}");
 }
 
@@ -40,6 +38,41 @@ fn total_points_of_scratchers(scratchcards: &str) -> u64 {
         .sum()
 }
 
+fn how_many_after_playing(scratchcards: &str) -> u64 {
+    let mut copies = [0u32; 194]; // there are max 193 cards by id. Add 1 to fix offsets
+
+    for card in scratchcards.lines() {
+        let (header, numbers) = card.split_once(':').expect("all cards have a header");
+        let this_card = header
+            .strip_prefix("Card")
+            .unwrap()
+            .trim()
+            .parse::<usize>()
+            .unwrap();
+        copies[this_card] += 1;
+
+        let (winning_numbers_str, your_numbers_str) = numbers
+            .split_once('|')
+            .expect("all cards have two sections of numbers");
+        let winning_numbers: HashSet<u32> = winning_numbers_str
+            .split_ascii_whitespace()
+            .map(|num| num.parse::<u32>().unwrap())
+            .collect();
+        let num_winners = your_numbers_str
+            .split_ascii_whitespace()
+            .map(|num| num.parse::<u32>().unwrap())
+            .filter(|num| winning_numbers.contains(num))
+            .count();
+        if num_winners > 0 {
+            let copies_of_this_card = copies[this_card];
+            for offset in 1..=num_winners {
+                copies[this_card + offset] += copies_of_this_card;
+            }
+        }
+    }
+
+    copies.iter().map(|i| u64::from(*i)).sum()
+}
 #[cfg(test)]
 mod test {
 
@@ -58,7 +91,12 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
     #[test]
     fn test_case_part_2() {
-        let input = "";
-        assert_eq!(2, 2);
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        assert_eq!(how_many_after_playing(input), 30);
     }
 }
