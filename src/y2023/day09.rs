77d85379a35166f023445f184a8695a90c53571c
Sam Clark <sam.h.clark@gmail.com>
2023-12-09 19:50:13 -0600

    2023, day 9, part 2

--- src/y2023/day09.rs
+++ src/y2023/day09.rs
@@ -13,19 +13,31 @@ pub fn part01() {
 
 #[allow(dead_code)]
 pub fn part02() {
-    let answer = "";
+    let answer = sum_of_extrapolations(INPUT);
     println!("Puzzle answer: {answer}");
 }
 
 fn sum_of_predictions(report: &str) -> i64 {
     report
-        .lines()
+        .par_lines()
         .map(|line| {
             line.split_ascii_whitespace()
                 .map(|s| s.parse::<i64>().expect("all input values will parse"))
                 .collect::<Vec<i64>>()
         })
         .map(|values| predict_next_value(&values))
+        .sum()
+}
+
+fn sum_of_extrapolations(report: &str) -> i64 {
+    report
+        .par_lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|s| s.parse::<i64>().expect("all input values will parse"))
+                .collect::<Vec<i64>>()
+        })
+        .map(|values| extrapolate_previous_value(&values))
         .sum()
 }
 
@@ -61,6 +73,36 @@ fn predict_next_value(values: &[i64]) -> i64 {
     *work.get(0).unwrap().last().unwrap()
 }
 
+fn extrapolate_previous_value(values: &[i64]) -> i64 {
+    let max_layers = values.len() - 1;
+    let mut work: Vec<Vec<i64>> = Vec::with_capacity(max_layers);
+
+    // insert first layer
+    work.push(values.to_vec());
+
+    // build the rest of the layers
+    loop {
+        let next_layer = next_layer(work.last().unwrap());
+        work.push(next_layer.clone());
+        if layer_is_all_zeros(&next_layer) {
+            break;
+        }
+    }
+
+    // working from the bottom up, add the extrapolated value to each layer
+    let num_layers = work.len();
+    let mut first_val_in_prev_layer = 0;
+    for i in 1..=num_layers {
+        let layer = work.get_mut(num_layers - i).unwrap();
+        let first_val_in_this_layer = *layer.first().unwrap();
+        let extrapolation_for_layer = first_val_in_this_layer - first_val_in_prev_layer;
+        layer.insert(0, extrapolation_for_layer);
+        first_val_in_prev_layer = extrapolation_for_layer;
+    }
+
+    *work.get(0).unwrap().first().unwrap()
+}
+
 fn next_layer(values: &[i64]) -> Vec<i64> {
     values.iter().tuple_windows().map(|(a, b)| b - a).collect()
 }
@@ -83,8 +125,16 @@ mod test {
     }
 
     #[test]
-    fn test_case_part_2() {
-        let input = "";
-        assert_eq!(6, 6);
+    fn test_case_part_2_small() {
+        let input = "10 13 16 21 30 45";
+        assert_eq!(sum_of_extrapolations(input), 5);
+    }
+
+    #[test]
+    fn test_case_part_2_full() {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(sum_of_extrapolations(input), 2);
     }
 }
